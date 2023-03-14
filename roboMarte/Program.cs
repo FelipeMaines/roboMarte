061fff2841d3b820a112dfc2a051f2cd004e1e6d
Felipe Maines <felipemaines123@gmail.com>
2023-03-14 17:42:38 -0300

    Add o do-While

--- roboMarte/Program.cs
+++ roboMarte/Program.cs
@@ -13,320 +13,193 @@
 
             //Declarando o tamanho da matriz
 
-            string[] matriz = new string[100];
-            Console.WriteLine("Qual o tamnho do espaco:");
-            string tamanho = Console.ReadLine();
-
-            matriz = tamanho.Split(' ');
-
-            int intMatrizXPrimeiro = int.Parse(matriz[0]);
-            int intMatrizYPrimeiro = int.Parse(matriz[1]);
 
 
-            string[] inicialPrimeiro = new string[100];
-            Console.WriteLine("Qual a posicao inicial: ");
-            string posicaoInicialPrimeiro = Console.ReadLine();
-
-            inicialPrimeiro = posicaoInicialPrimeiro.Split(' ');
-
-
-            xPrimeiro = int.Parse(inicialPrimeiro[0]);
-            yPrimeiro = int.Parse(inicialPrimeiro[1]);
-
-
-
-
-            //Dando Norte eh igual ou a 0 ou a 360 / L = 90 / S = 180 / O = 270
-
-            switch (inicialPrimeiro[2])
+            int contador = 0;
+            do
             {
-                case "N":
-                    valorDirecao = 0;
-                    break;
+                string[] matriz = new string[2];
+                Console.WriteLine("Qual o tamnho do espaco:");
+                string tamanho = Console.ReadLine();
 
-                case "L":
-                    valorDirecao = 90;
-                    break;
+                matriz = tamanho.Split(' ');
 
-                case "S":
-                    valorDirecao = 180;
-                    break;
-
-                case "O":
-                    valorDirecao = 270;
-                    break;
+                int intMatrizXPrimeiro = int.Parse(matriz[0]);
+                int intMatrizYPrimeiro = int.Parse(matriz[1]);
 
 
-                default:
-                    Console.WriteLine("Erro");
-                    break;
-            }
+                string[] inicialPrimeiro = new string[3];
+                Console.WriteLine("Qual a posicao inicial: ");
+                string posicaoInicialPrimeiro = Console.ReadLine();
+
+                inicialPrimeiro = posicaoInicialPrimeiro.Split(' ');
 
 
-            Console.WriteLine("Qual sao os comandos dados: ");
-            string strcomandos = Console.ReadLine();
+                xPrimeiro = int.Parse(inicialPrimeiro[0]);
+                yPrimeiro = int.Parse(inicialPrimeiro[1]);
 
-            char[] comandos = new char[50];
-
-            comandos = strcomandos.ToCharArray();
-
-            foreach (char comando in comandos)
-            {
-                if (comando == 'E')
+                switch (inicialPrimeiro[2])
                 {
-
-                    if (valorDirecao == 0)
-                    {
-                        valorDirecao = 360;
-                    }
-
-                    valorDirecao -= 90;
-                }
-
-                else if (comando == 'D')
-                {
-                    if (valorDirecao == 360)
-                    {
+                    case "N":
                         valorDirecao = 0;
-                    }
+                        break;
 
-                    valorDirecao += 90;
+                    case "L":
+                        valorDirecao = 90;
+                        break;
+
+                    case "S":
+                        valorDirecao = 180;
+                        break;
+
+                    case "O":
+                        valorDirecao = 270;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Erro");
+                        break;
+
+
                 }
 
-                if (comando == 'M')
+                Console.WriteLine("Qual sao os comandos dados: ");
+                string strcomandos = Console.ReadLine();
+
+                char[] comandos = new char[50];
+
+                comandos = strcomandos.ToCharArray();
+
+                bool isOut = false;
+
+                foreach (char comando in comandos)
                 {
-                    if (valorDirecao == 0 || valorDirecao == 360)
+                    if (isOut)
                     {
-                        if (yPrimeiro > intMatrizYPrimeiro)
+                        break;
+                    }
+                    if (comando == 'E')
+                    {
+
+                        if (valorDirecao == 0)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Robo saiu do espaco de trabalho!");
-                            break;
+                            valorDirecao = 360;
                         }
 
-                        yPrimeiro += 1;
+                        valorDirecao -= 90;
                     }
 
-                    else if (valorDirecao == 90)
+                    else if (comando == 'D')
                     {
-                        if (xPrimeiro > intMatrizXPrimeiro)
+                        if (valorDirecao == 360)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Robo saiu do espaco de trabalho!");
-                            break;
+                            valorDirecao = 0;
                         }
-                        xPrimeiro += 1;
+
+                        valorDirecao += 90;
                     }
 
-                    else if (valorDirecao == 180)
+                    if (comando == 'M')
                     {
-                        if (yPrimeiro == 0)
+                        if (valorDirecao == 0 || valorDirecao == 360)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("O robo saiu da area! ");
-                            break;
-                        }
-                        yPrimeiro -= 1;
-                    }
 
-                    else if (valorDirecao == 270)
-                    {
-                        if (yPrimeiro == 0)
+                            yPrimeiro += 1;
+
+                            if (yPrimeiro > intMatrizYPrimeiro)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Robo saiu do espaco de trabalho!");
+                                isOut = true;
+                                contador++;
+                                continue;
+                            }
+
+                        }
+
+                        else if (valorDirecao == 90)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("O robo saiu da area! ");
-                            break;
-                        }
-                        xPrimeiro -= 1;
-                    }
+                            xPrimeiro += 1;
 
+                            if (xPrimeiro > intMatrizXPrimeiro)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Robo saiu do espaco de trabalho!");
+                                contador++;
+                                continue;
+                            }
+                        }
+
+                        else if (valorDirecao == 180)
+                        {
+                            yPrimeiro -= 1;
+
+                            if (yPrimeiro == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("O robo saiu da area! ");
+                                contador++;
+                                continue;
+                            }
+                        }
+
+                        else if (valorDirecao == 270)
+                        {
+                            xPrimeiro -= 1;
+
+                            if (yPrimeiro == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("O robo saiu da area! ");
+                                contador++;
+                                continue;
+                            }
+                        }
+
+                    }
                 }
-            }
-
-            switch (valorDirecao)
-            {
-                case 0:
-                    olhando = 'N';
-                    break;
-
-                case 360:
-                    olhando = 'N';
-                    break;
-
-                case 90:
-                    olhando = 'L';
-                    break;
-
-                case 180:
-                    olhando = 'S';
-                    break;
-
-                case 270:
-                    olhando = 'O';
-                    break;
-            }
 
 
-            //Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{xPrimeiro} {yPrimeiro} {olhando}");
 
-            Console.ForegroundColor = ConsoleColor.White;
-
-            //Segundo Robo --------------------------------------------------------------------------------------
-
-            string[] inicialSegundo = new string[100];
-            Console.WriteLine("Qual a posicao inicial do segundo robo: ");
-            string posicaoInicialSegundo = Console.ReadLine();
-
-            inicialSegundo = posicaoInicialSegundo.Split(' ');
-
-            xSegundo = int.Parse(inicialSegundo[0]);
-            ySegundo = int.Parse(inicialSegundo[1]);
-
-            switch (inicialSegundo[2])
-            {
-                case "N":
-                    valorDirecaoSeg = 0;
-                    break;
-
-                case "L":
-                    valorDirecaoSeg = 90;
-                    break;
-
-                case "S":
-                    valorDirecaoSeg = 180;
-                    break;
-
-                case "O":
-                    valorDirecaoSeg = 270;
-                    break;
-
-
-                default:
-                    Console.WriteLine("Erro");
-                    break;
-            }
-
-
-            Console.WriteLine("Qual sao os comandos dados: ");
-            string strcomandosSeg = Console.ReadLine();
-
-            char[] comandosSeg = new char[50];
-
-            comandosSeg = strcomandosSeg.ToCharArray();
-
-
-            foreach (char comando in comandosSeg)
-            {
-                if (comando == 'E')
+                switch (valorDirecao)
                 {
+                    case 0:
+                        olhando = 'N';
+                        break;
 
-                    if (valorDirecaoSeg == 0)
-                    {
-                        valorDirecaoSeg = 360;
-                    }
+                    case 360:
+                        olhando = 'N';
+                        break;
 
-                    valorDirecaoSeg -= 90;
+                    case 90:
+                        olhando = 'L';
+                        break;
+
+                    case 180:
+                        olhando = 'S';
+                        break;
+
+                    case 270:
+                        olhando = 'O';
+                        break;
                 }
 
-                else if (comando == 'D')
+                if (!isOut)
                 {
-                    if (valorDirecaoSeg == 360)
-                    {
-                        valorDirecaoSeg = 0;
-                    }
-
-                    valorDirecaoSeg += 90;
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{xPrimeiro} {yPrimeiro} {olhando}");
+                    contador++;
                 }
-
-                if (comando == 'M')
-                {
-                    if (valorDirecaoSeg == 0 || valorDirecaoSeg == 360)
-                    {
-                        if (ySegundo > 5)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Robo saiu do espaco de trabalho!");
-                            break;
-                        }
-
-                        ySegundo += 1;
-                    }
-
-                    else if (valorDirecaoSeg == 90)
-                    {
-                        if (xSegundo > 5)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Robo saiu do espaco de trabalho!");
-                            break;
-                        }
-                        xSegundo += 1;
-                    }
-
-                    else if (valorDirecaoSeg == 180)
-                    {
-                        if (ySegundo == 0)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("O robo saiu da area! ");
-                            break;
-                        }
-                        ySegundo -= 1;
-                    }
-
-                    else if (valorDirecaoSeg == 270)
-                    {
-                        if (ySegundo == 0)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("O robo saiu da area! ");
-                            break;
-                        }
-                        xSegundo -= 1;
-                    }
-
-                }
-            }
-
-            char olhandoSeg = 'f';
-
-            switch (valorDirecaoSeg)
-            {
-                case 0:
-                    olhandoSeg = 'N';
-                    break;
-
-                case 360:
-                    olhandoSeg = 'N';
-                    break;
-
-                case 90:
-                    olhandoSeg = 'L';
-                    break;
-
-                case 180:
-                    olhandoSeg = 'S';
-                    break;
-
-                case 270:
-                    olhandoSeg = 'O';
-                    break;
-            }
-
-
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{xSegundo} {ySegundo} {olhandoSeg}");
-
-            Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.White;
 
 
 
-
-
+            } while (contador != 2);
 
         }
     }
 }
+
+
